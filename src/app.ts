279ebea9172e561e4d153bbcb0ccaef533cd2011
Max Dickinson <maxsantistadickinson@gmail.com>
2024-08-07 11:35:33 -0300

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import { limiter } from "./middlewares/rate-limit";
 import { bodyParser } from "./middlewares/json";
 import { routes } from "./routes";
 import { error } from "./middlewares/error";
+import { cors } from "./middlewares/cors";
 
 const app = express();
 
+app.use(cors);
 app.use(limiter);
 app.use(bodyParser);
 app.use(routes);
